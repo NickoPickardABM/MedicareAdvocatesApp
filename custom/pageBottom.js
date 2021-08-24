fb6515c90335d2ca8db5b0d69199d07338f47bb6
NickoPickardABM <87043091+NickoPickardABM@users.noreply.github.com>
2021-08-24 16:43:45 -0600

    Update pageBottom.js

--- custom/pageBottom.js
+++ custom/pageBottom.js
@@ -5,7 +5,7 @@ redocument.write('<div id="bottomBar">');
 document.write('<img src="custom/logo.jpg" alt="Twilio"/>');
 document.write('<div id="bottomContent">');
 document.write('<div style="padding-bottom: 10px;"><a href="https://medicareadvocates.com/" style="color: rgba(255,255,255,.65);">Medicare Advocates</a></div>');
-document.write(' | <a href="mailto:nicko@agentboost.com" style="color: rgba(255,255,255,.65);">For Support, Contact Nicko Pickard</a>');
+document.write('<a href="mailto:nicko@agentboost.com" style="color: rgba(255,255,255,.65);">For Support, Contact Nicko Pickard</a>');
 document.write('</div>');
 
 document.write('<div style="padding-top: 10px;">2021 Medicare Advocates</div>');
