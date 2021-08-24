c572aa8172f86653ec2bc7135bc5ecded3210f98
NickoPickardABM <87043091+NickoPickardABM@users.noreply.github.com>
2021-08-24 14:26:15 -0600

    Update pageBottom.js

--- custom/pageBottom.js
+++ custom/pageBottom.js
@@ -8,5 +8,5 @@ document.write('<div style="padding-bottom: 10px;"><a href="https://medicareadvo
 document.write(' | <a href="mailto:nicko@agentboost.com" style="color: rgba(255,255,255,.65);">For Support, Contact Nicko Pickard</a>');
 document.write('</div>');
 
-document.write('<div style="padding-top: 10px;">2018 Twilio</div>');
+document.write('<div style="padding-top: 10px;">2021 Medicare Advocates</div>');
 document.write('</div>');
