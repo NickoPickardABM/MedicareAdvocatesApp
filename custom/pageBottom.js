0132c4fcb2da2e6dcd12866fc3844cd01b0599e6
NickoPickardABM <87043091+NickoPickardABM@users.noreply.github.com>
2021-08-24 14:25:52 -0600

    Update pageBottom.js

--- custom/pageBottom.js
+++ custom/pageBottom.js
@@ -1,13 +1,11 @@
-document.write('<div id="bottomBar">');
+redocument.write('<div id="bottomBar">');
 // document.write('<hr>');
 // Centered:
 // Logo, just the logo.
 document.write('<img src="custom/logo.jpg" alt="Twilio"/>');
 document.write('<div id="bottomContent">');
-document.write('<div style="padding-bottom: 10px;"><a href="https://github.com/tigerfarm/owlcc" style="color: rgba(255,255,255,.65);">GitHub project</a></div>');
-document.write('<a href="https://twilio.learnupon.com/store" style="color: rgba(255,255,255,.65);">Training</a>');
-document.write(' | <a href="https://www.twilio.com/support-plans" style="color: rgba(255,255,255,.65);">Support</a>');
-document.write(' | <a href="https://www.twilio.com/company" style="color: rgba(255,255,255,.65);">About Twilio</a>');
+document.write('<div style="padding-bottom: 10px;"><a href="https://medicareadvocates.com/" style="color: rgba(255,255,255,.65);">Medicare Advocates</a></div>');
+document.write(' | <a href="mailto:nicko@agentboost.com" style="color: rgba(255,255,255,.65);">For Support, Contact Nicko Pickard</a>');
 document.write('</div>');
 
 document.write('<div style="padding-top: 10px;">2018 Twilio</div>');
